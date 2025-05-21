3890adde272483525894cfc75a6aa4422cfe1b5e
Ruslan1908 <92510484+Ruslan1908@users.noreply.github.com>
2025-05-21 19:29:59 +0200

    Update config.py

--- tiles/config.py
+++ tiles/config.py
@@ -1,1 +1,13 @@
+# Konfiguracja ekranu, FPS, kolory i inne ustawienia gry
 
+SCREEN_WIDTH = 800         # szerokość okna gry
+SCREEN_HEIGHT = 600        # wysokość okna gry
+FPS = 60                   # liczba klatek na sekundę
+
+# Kolory w formacie (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE  = (0, 0, 255)
+CYAN  = (0, 255, 255)
